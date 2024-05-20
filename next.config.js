0bf3b9fa493c6fb082a7a7f72eaf721dd88a25a8
Alexis Guzman <codingcodax@gmail.com>
2024-05-20 16:28:28 -0600

    feat(nextconfig): add pwa config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
+import withSerwistInit from "@serwist/next";
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.js");
 
-/** @type {import("next").NextConfig} */
-const config = {};
+const revision = crypto.randomUUID();
 
-export default config;
+const withSerwist = withSerwistInit({
+  cacheOnNavigation: true,
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  additionalPrecacheEntries: [{ url: "/~offline", revision }],
+});
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+};
+
+export default withSerwist(config);
