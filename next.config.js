c06b26159a05983ead23a07a6b0232577dfec532
Alexis Guzman <codingcodax@gmail.com>
2024-05-20 17:09:08 -0600

    fix(nextconfig): update sw src

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const revision = crypto.randomUUID();
 
 const withSerwist = withSerwistInit({
   cacheOnNavigation: true,
-  swSrc: "app/sw.ts",
+  swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
   additionalPrecacheEntries: [{ url: "/~offline", revision }],
 });
