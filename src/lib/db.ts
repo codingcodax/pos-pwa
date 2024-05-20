0af4f2f86aaadc4695b42dbc820b12ccf096374f
Alexis Guzman <codingcodax@gmail.com>
2024-05-20 12:52:38 -0600

    feat(lib): create db class

---
+++ src/lib/db.ts
@@ -0,0 +1,44 @@
+import Dexie, { type Table } from "dexie";
+import { populate } from "./populate";
+
+export type TodoItem = {
+  id?: number;
+  todoListId: number;
+  title: string;
+  done?: boolean;
+};
+
+export type TodoList = {
+  id?: number;
+  title: string;
+};
+
+export class TodoDB extends Dexie {
+  todoLists!: Table<TodoList, number>;
+  todoItems!: Table<TodoItem, number>;
+  constructor() {
+    super("TodoDB");
+    this.version(1).stores({
+      todoLists: "++id",
+      todoItems: "++id, todoListId",
+    });
+  }
+
+  deleteList(todoListId: number) {
+    return this.transaction("rw", this.todoItems, this.todoLists, () => {
+      this.todoItems.where({ todoListId }).delete();
+      this.todoLists.delete(todoListId);
+    });
+  }
+}
+
+export const db = new TodoDB();
+
+db.on("populate", populate);
+
+export function resetDatabase() {
+  return db.transaction("rw", db.todoLists, db.todoItems, async () => {
+    await Promise.all(db.tables.map((table) => table.clear()));
+    await populate();
+  });
+}
