2d60fc751c0617d6b9fe927bdcb47b3bb83b313f
Alexis Guzman <codingcodax@gmail.com>
2024-05-21 17:14:10 -0600

    feat(components): setup status banner

--- src/app/_components/StatusBanner.tsx
+++ src/app/_components/StatusBanner.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import clsx from "clsx/lite";
+import { useNetwork } from "~/hooks/useNetwork";
+
 export const StatusBanner = () => {
+  const { isOnline } = useNetwork();
+
   return (
-    <div>
-      <p>status banner</p>
+    <div
+      className={clsx(
+        "flex w-full items-center justify-center p-2 font-medium",
+        !isOnline ? "bg-red-400" : "bg-green-400",
+      )}
+    >
+      {isOnline ? "Online" : "Offline"}
     </div>
   );
 };
