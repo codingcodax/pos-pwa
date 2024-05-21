9a9756fbf2262e212cf04ad611e66deb1202fafe
Alexis Guzman <codingcodax@gmail.com>
2024-05-21 10:02:24 -0600

    feat(pages): add form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,77 @@
 "use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import type { NextPage } from "next";
-import { useLiveQuery } from "dexie-react-hooks";
-import { db } from "~/lib/db";
-import { api } from "~/trpc/react";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+
+const orderSchema = z.object({
+  total: z.string(),
+  items: z.string(),
+});
+
+type OrderType = z.infer<typeof orderSchema>;
 
 const Home: NextPage = () => {
-  const { data: hello } = api.todo.hello.useQuery();
-  const lists = useLiveQuery(() => db.todoLists.toArray());
+  const form = useForm<OrderType>({
+    resolver: zodResolver(orderSchema),
+    defaultValues: {
+      total: "",
+      items: "",
+    },
+  });
 
-  console.log("📋 ~ lists:", lists);
-  console.log("📋 ~ hello:", hello);
+  const onSubmit = (data: OrderType) => {
+    console.log(data);
+  };
 
   return (
     <div>
-      <p>home page</p>
-      <button onClick={() => db.deleteList(1)}>Db</button>
+      <p>online page</p>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="total"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Total</FormLabel>
+                <FormControl>
+                  <Input placeholder="10" type="number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="items"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Items</FormLabel>
+                <FormControl>
+                  <Input placeholder="hamburger" type="string" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button>Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 };
