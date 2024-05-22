810fe032b6f4656f3cb2b59d56e287d572e79928
Alexis Guzman <codingcodax@gmail.com>
2024-05-21 18:14:32 -0600

    feat(sw): add online and offline event listeners

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,9 +1,10 @@
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { Serwist } from "serwist";
+import { db } from "~/lib/db";
 
 declare global {
-  interface WorkerGlobalScope extends SerwistGlobalConfig {
+  interface WorkerGlobalScope extends SerwistGlobalConfig, EventTarget {
     // Change this attribute's name to your `injectionPoint`.
     // `injectionPoint` is an InjectManifest option.
     // See https://serwist.pages.dev/docs/build/configuring
@@ -30,5 +31,22 @@ const serwist = new Serwist({
     ],
   },
 });
+
+self.addEventListener("online", async () => {
+  console.log("online");
+
+  const orders = await db.orders.toArray();
+  const req = await fetch("/api/hello", {
+    method: "GET",
+  });
+  const data = await req.json();
+
+  console.log(orders);
+  console.log(data);
+
+  console.log("finished");
+});
+
+self.addEventListener("offline", () => console.log("offline"));
 
 serwist.addEventListeners();
